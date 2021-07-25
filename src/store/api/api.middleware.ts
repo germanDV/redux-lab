7fddbd44ca1b89f1d633c28a98a8efc3c571b9eb
German Viescas <germanviescas@gmail.com>
2021-07-25 16:45:05 -0300

    Move axios instance to api middleware

--- src/store/api/api.middleware.ts
+++ src/store/api/api.middleware.ts
@@ -1,7 +1,12 @@
 import { Middleware, AnyAction } from 'redux'
-import axios, { Method } from '../../api/axios'
+import axios, { Method } from 'axios'
 import { RootState } from '../store'
 import { apiSuccess, apiError, isApiRequestAction } from './api.actions'
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:4000',
+  timeout: 5000,
+});
 
 export const apiMdw: Middleware<{}, RootState> = (store) => (next) => async (action: AnyAction) => {
   next(action)
@@ -10,11 +15,10 @@ export const apiMdw: Middleware<{}, RootState> = (store) => (next) => async (act
     const { method, url, domain } = action.meta
     try {
       const mthd = method.toLowerCase() as Method
-      const resp = await axios.request({ method: mthd, url })
+      const resp = await axiosInstance.request({ method: mthd, url })
       store.dispatch(apiSuccess({ domain, response: resp.data }))
     } catch (error) {
       store.dispatch(apiError({ domain, error }))
     }
   }
 }
-
